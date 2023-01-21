884b6fe898690f69c223fbe3369b9a364544d5e1
Miran-Amer <thesweatyog123@gmail.com>
2023-01-21 14:55:04 +0000

    First Commit

---
+++ extension.js
@@ -0,0 +1,60 @@
+const vscode = require('vscode');
+const axios = require("axios");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+
+	
+	console.log('Congratulations, your extension "bigo" is now active!');
+
+	
+	let disposable = vscode.commands.registerCommand('bigo.BigO', function () {
+		
+		vscode.window.showInformationMessage('Hello World from BigO!');
+		
+	});
+
+	let copyTextCommand = vscode.commands.registerCommand("bigo.copyText", function () {
+		
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor.selection;
+		if (selection && !selection.isEmpty) {
+			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+			const highlighted = editor.document.getText(selectionRange);
+			//const promptCode = highlighted.replace(/\n/g, ' ')
+			console.log(highlighted.replaceAll(' ', ''));
+
+			vscode.window.showInformationMessage("Successfully Selected Highlighted Text");
+			
+			const url = "http://localhost:8080/time-complexity";
+
+			const options = {
+				url: url,
+				method: 'POST',
+				headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json;charset=UTF-8'
+				},
+				data: {
+				"prompt": `${highlighted.replaceAll(' ', '')}`
+				}
+			};
+			
+			axios(options)
+				.then(response => {
+				console.log(response.data);
+				});
+			}
+	})
+
+	context.subscriptions.push(disposable, copyTextCommand);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
