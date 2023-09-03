10e9ae30e7b70858f49f816ea42e4a80235c0a39
Miran-Amer <thesweatyog123@gmail.com>
2023-09-03 16:33:56 +0100

    Added Option To Add API Key

--- extension.js
+++ extension.js
@@ -1,287 +1,268 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 const axios = require("axios");
-const config = require('./keys_config.json');
-const apiKey = config.apiKey;
+const { Configuration, OpenAIApi, OpenAI } = require("openai");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+  console.log('Congratulations, your extension "bigo" is now active!');
 
-	
-	console.log('Congratulations, your extension "bigo" is now active!');
+  // Function to set the API key
+  async function setApiKey() {
+    const apiKey1 = await vscode.window.showInputBox({
+      prompt: "Please enter your API key",
+      placeHolder: "API key",
+      ignoreFocusOut: true,
+    });
+  
+    if (apiKey1) {
+      const configuration = vscode.workspace.getConfiguration('bigo');
+      const storedApiKey = configuration.get('apiKey');
+  
+      // Update the apiKey property with the entered value
+      storedApiKey.apiKey = apiKey1;
+  
+      // Update the configuration with the modified object
+      configuration.update('apiKey', storedApiKey, vscode.ConfigurationTarget.Global).then(() => {
+        vscode.window.showInformationMessage("API key saved successfully!");
+        const apiKey = configuration.get("apiKey");
+        console.log("Stored: ", apiKey, "Entered: ", apiKey1);
+      });
+    }
+  }
 
-	
-	let disposable = vscode.commands.registerCommand('bigo.BigO', function () {
-		
-		vscode.window.showInformationMessage('Hello World from BigO!');
-		
-	});
+  // Register the "bigo.setApiKey" command
+  let setApiKeyCommand = vscode.commands.registerCommand(
+    "bigo.setApiKey",
+    setApiKey
+  );
 
-	let copyTextCommand = vscode.commands.registerCommand("bigo.copyText", function () {
-		
-		const editor = vscode.window.activeTextEditor;
-		const selection = editor.selection;
-		if (selection && !selection.isEmpty) {
-			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-			const highlighted = editor.document.getText(selectionRange);
-			//const promptCode = highlighted.replace(/\n/g, ' ')
-			//console.log(highlighted.replaceAll(' ', ''));
-			
-			const url = "https://vscodeextensionserver.onrender.com/time-complexity";
+  // Event listener for changes in the extension configuration
+  vscode.extensions.onDidChange(() => {
+    const configuration = vscode.workspace.getConfiguration();
+    const apiKey = configuration.get("apiKey");
+  });
 
-			const options = {
-				url: url,
-				method: 'POST',
-				headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json;charset=UTF-8'
-				},
-				data: {
-				"prompt": `${highlighted.replaceAll(' ', '')}`
-				}
-			};
-			
-			axios(options)
-				.then(response => {
-				//console.log(response.data);
-				vscode.window.showInformationMessage(`Time Complexity => ${response.data.data}`);
-				});
-			}
-	})
+  // Set the API key initially
+  setApiKey();
 
-	let optimise = vscode.commands.registerCommand('bigo.optimizeCode', async function () {
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No editor is active');
-			return;
-		}
+  function getApiKey() {
+    const configuration = vscode.workspace.getConfiguration('bigo');
+    const apiKeyObject = configuration.get('apiKey');
+  
+    // Retrieve the API key string from the 'apiKey' property
+    return apiKeyObject.apiKey;
+  }
+  
+  let displayApiKey = vscode.commands.registerCommand("bigo.displayApiKey", function () {
+    const apiKey = getApiKey();
+    
+    if (apiKey) {
+      vscode.window.showInformationMessage(`Stored API Key: ${apiKey}`);
+    } else {
+      vscode.window.showErrorMessage("No API Key stored.");
+    }
+  });
 
-		// Get the selected text
-		var selection = editor.selection;
-		var text = editor.document.getText(selection);
+  const generateOpenAIResponse = async (prompt) => {
+    //const openai_config = new OpenAI({
+      //apiKey: getApiKey(),
+    //});
 
-		//console.log(text)
+    //const openai_instance = new OpenAI.OpenAIApi(openai_config);
 
-		// Send the selected text to the OpenAI API
-		await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-			prompt: `Make the following code as efficient as possible and as fast as possible.
-					${text.replaceAll(' ', '')}
-					###
-					`,
-			temperature: 0.5,
-			max_tokens: 2048,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
-		}, {
-			headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${apiKey}`
-			}
-		})
-			.then(response => {
-			// Get the optimized code snippet from the API response
-			var optimizedCode = response.data.choices[0].text;
-			//console.log(optimizedCode)
+    const OAI = new OpenAI({
+      apiKey: getApiKey(),
+    });
+    
+    const completion = await OAI.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      max_tokens: 2048,
+    });
 
-			// Create a new editor window and show it
-			vscode.workspace.openTextDocument({ content: optimizedCode }).then(doc => {
-				vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
-			});
-		});
-	})
+    console.log(completion);
 
-	let readability = vscode.commands.registerCommand('bigo.readableCode', async function () {
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No editor is active');
-			return;
-		}
-
-		// Get the selected text
-		var selection = editor.selection;
-		var text = editor.document.getText(selection);
-
-		var readableCode;
-
-		//console.log(text)
-
-		// Send the selected text to the OpenAI API
-		await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-			prompt: `Make the following code as readable as possible and add any necessary comments to explain it.
-					${text.replaceAll(' ', '')}
-					###
-					`,
-			temperature: 0.5,
-			max_tokens: 2048,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
-		}, {
-			headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${apiKey}`
-			}
-		})
-			.then(response => {
-			// Get the optimized code snippet from the API response
-			readableCode = response.data.choices[0].text;
-			//console.log(readableCode)
-
-		});
-
-		// Create a new editor window and show it
-		vscode.workspace.openTextDocument({ content: readableCode }).then(doc => {
-			vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
-		});
-	})
-
-	let debugging = vscode.commands.registerCommand('bigo.debugCode', async function () {
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No editor is active');
-			return;
-		}
-
-		// Get the selected text
-		var selection = editor.selection;
-		var text = editor.document.getText(selection);
-
-		var debuggedCode;
-
-		//console.log(text)
-
-		// Send the selected text to the OpenAI API
-		await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-			prompt: `Debug the following code by fixing the errors.
-					${text.replaceAll(' ', '')}
-					###
-					`,
-			temperature: 0.5,
-			max_tokens: 2048,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
-		}, {
-			headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${apiKey}`
-			}
-		})
-			.then(response => {
-			// Get the optimized code snippet from the API response
-			debuggedCode = response.data.choices[0].text;
-			//console.log(debuggedCode)
-
-		});
-
-		// Create a new editor window and show it
-		vscode.workspace.openTextDocument({ content: debuggedCode }).then(doc => {
-			vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
-		});
-	})
+    return completion.choices[0].message.content
+  }
 
 
-	let converter = vscode.commands.registerCommand('bigo.convertCode', async function () {
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No editor is active');
-			return;
-		}
+  let copyTextCommand = vscode.commands.registerCommand(
+    "bigo.copyText",
+    async function () {
+      const editor = vscode.window.activeTextEditor;
+      const selection = editor.selection;
+      if (selection && !selection.isEmpty) {
+        const selectionRange = new vscode.Range(
+          selection.start.line,
+          selection.start.character,
+          selection.end.line,
+          selection.end.character
+        );
+        const highlighted = editor.document.getText(selectionRange);
 
-		// Get the selected text
-		var selection = editor.selection;
-		var text = editor.document.getText(selection);
+        const prompt = 'whats the Big O complexity of this code: ' + highlighted.replaceAll(" ", "");
+        
+        const response = await generateOpenAIResponse(prompt);
 
-		var convertedCode;
+        vscode.window.showInformationMessage(
+          `Time Complexity => ${response}`
+        );
 
-		//console.log(text)
+      }
+    }
+  );
 
-		// Send the selected text to the OpenAI API
-		await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-			prompt: `Convert the following code into the commented programming language placed on top of the code.
-					${text.replaceAll(' ', '')}
-					###
-					`,
-			temperature: 0.5,
-			max_tokens: 2048,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
-		}, {
-			headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${apiKey}`
-			}
-		})
-			.then(response => {
-			// Get the optimized code snippet from the API response
-			convertedCode = response.data.choices[0].text;
-			//console.log(convertedCode)
+  let optimise = vscode.commands.registerCommand(
+    "bigo.optimizeCode",
+    async function () {
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No editor is active");
+        return;
+      }
 
-		});
+      // Get the selected text
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
+  
+      const prompt = 'Make the following code as efficient as possible and as fast as possible: ' + text.replaceAll(" ", "");
+        
+      const response = await generateOpenAIResponse(prompt);
 
-		// Create a new editor window and show it
-		vscode.workspace.openTextDocument({ content: convertedCode }).then(doc => {
-			vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
-		});
-	})
+      // Send the selected text to the OpenAI API
+      vscode.workspace
+            .openTextDocument({ content: response })
+            .then((doc) => {
+              vscode.window.showTextDocument(doc, {
+                viewColumn: vscode.ViewColumn.Beside,
+              });
+      });
+    }
+  );
 
+  let readability = vscode.commands.registerCommand(
+    "bigo.readableCode",
+    async function () {
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No editor is active");
+        return;
+      }
 
-	let solver = vscode.commands.registerCommand('bigo.solveCode', async function () {
-		var editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No editor is active');
-			return;
-		}
+      // Get the selected text
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
 
-		// Get the selected text
-		var selection = editor.selection;
-		var text = editor.document.getText(selection);
+      const prompt = 'Make the following code as readable as possible and add any necessary comments to explain it: ' + text.replaceAll(" ", "");
+        
+      const response = await generateOpenAIResponse(prompt);
 
-		var solvedCode;
+      vscode.workspace
+        .openTextDocument({ content: response })
+        .then((doc) => {
+          vscode.window.showTextDocument(doc, {
+            viewColumn: vscode.ViewColumn.Beside,
+          });
+        });
+    }
+  );
 
-		//console.log(text)
+  let debugging = vscode.commands.registerCommand(
+    "bigo.debugCode",
+    async function () {
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No editor is active");
+        return;
+      }
 
-		// Send the selected text to the OpenAI API
-		await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-			prompt: `Write code to solve the following problem.
-					${text}
-					###
-					`,
-			temperature: 0.5,
-			max_tokens: 2048,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0
-		}, {
-			headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${apiKey}`
-			}
-		})
-			.then(response => {
-			// Get the optimized code snippet from the API response
-			solvedCode = response.data.choices[0].text;
-			//console.log(solvedCode)
+      // Get the selected text
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
 
-		});
+      const prompt = 'Debug the following code by fixing the errors: ' + text.replaceAll(" ", "");
+        
+      const response = await generateOpenAIResponse(prompt);
+      
+      vscode.workspace
+        .openTextDocument({ content: response })
+        .then((doc) => {
+          vscode.window.showTextDocument(doc, {
+            viewColumn: vscode.ViewColumn.Beside,
+          });
+        })
+    }
+  );
 
-		// Create a new editor window and show it
-		vscode.workspace.openTextDocument({ content: solvedCode }).then(doc => {
-			vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
-		});
-	})
+  let converter = vscode.commands.registerCommand(
+    "bigo.convertCode",
+    async function () {
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No editor is active");
+        return;
+      }
 
+      // Get the selected text
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
 
+      const prompt = 'Convert the following code into the commented programming language placed on top of the code: ' + text.replaceAll(" ", "");
+        
+      const response = await generateOpenAIResponse(prompt);
 
-	context.subscriptions.push(disposable, copyTextCommand, optimise, readability, debugging, converter, solver);
+      vscode.workspace
+        .openTextDocument({ content: response })
+        .then((doc) => {
+          vscode.window.showTextDocument(doc, {
+            viewColumn: vscode.ViewColumn.Beside,
+          });
+        })
+    }
+  );
+
+  let solver = vscode.commands.registerCommand(
+    "bigo.solveCode",
+    async function () {
+      var editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No editor is active");
+        return;
+      }
+
+      // Get the selected text
+      var selection = editor.selection;
+      var text = editor.document.getText(selection);
+
+      const prompt = 'Write code to solve the following problem: ' + text.replaceAll(" ", "");
+        
+      const response = await generateOpenAIResponse(prompt);
+
+      vscode.workspace.openTextDocument({ content: response }).then((doc) => {
+        vscode.window.showTextDocument(doc, {
+          viewColumn: vscode.ViewColumn.Beside,
+        });
+      })
+    }
+  );
+
+  context.subscriptions.push(
+    copyTextCommand,
+    optimise,
+    readability,
+    debugging,
+    converter,
+    solver,
+    setApiKeyCommand,
+    displayApiKey
+  );
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
